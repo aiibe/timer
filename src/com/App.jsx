8b069665a0cfeb44f084fd751a387454acfbfce2
aiibe <souksavanh.sy@gmail.com>
2020-05-05 22:51:58 +0700

    making a timer

--- src/com/App.jsx
+++ src/com/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { Play, Reset, Pause } from '../icons'
 
 export default function App() {
+	const interval = useRef()
+	const [time, setTime] = useState(25 * 60)
+	const [status, setStatus] = useState(false)
+
+	function toggleState() {
+		if (!status) {
+			setStatus(true)
+			interval.current = setInterval(() => {
+				setTime((time) => time - 1)
+			}, 1000)
+		} else {
+			setStatus(false)
+			clearInterval(interval.current)
+		}
+	}
+
 	return (
 		<div className='clock-frame'>
 			<div className='task'>
@@ -10,11 +26,15 @@ export default function App() {
 			<div className='digits'>
 				<div className='minutes'>00</div>
 				<div>:</div>
-				<div className='seconds'>00</div>
+				<div className='seconds'>{time}</div>
 			</div>
 			<div className='controls'>
-				<button className='button-rounded'>
-					<Play className='icon-button' />
+				<button className='button-rounded' onClick={toggleState}>
+					{status ? (
+						<Pause className='icon-button' />
+					) : (
+						<Play className='icon-button' />
+					)}
 				</button>
 				<button className='button-rounded'>
 					<Reset className='icon-button' />
