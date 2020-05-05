6940fd45cc22009386ef690cca20e74224fc4bc2
aiibe <souksavanh.sy@gmail.com>
2020-05-05 21:52:07 +0700

    custom layout

--- src/com/App.jsx
+++ src/com/App.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
 export default function App() {
-	return <div>Hello World !</div>
+	return (
+		<div className='clock-frame'>
+			<div className='task'>
+				<h3>Focus on this task</h3>
+			</div>
+			<div className='digits'>
+				<div className='minutes'>00</div>
+				<div>:</div>
+				<div className='seconds'>00</div>
+			</div>
+			<div className='controls'>
+				<button>Start</button>
+				<button>Reset</button>
+			</div>
+		</div>
+	)
 }
