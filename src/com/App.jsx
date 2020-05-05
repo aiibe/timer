94c51126b04ca592a42df59c882336d3179d403c
aiibe <souksavanh.sy@gmail.com>
2020-05-05 22:16:48 +0700

    added icons

--- src/com/App.jsx
+++ src/com/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Play, Reset, Pause } from '../icons'
 
 export default function App() {
 	return (
@@ -12,8 +13,12 @@ export default function App() {
 				<div className='seconds'>00</div>
 			</div>
 			<div className='controls'>
-				<button>Start</button>
-				<button>Reset</button>
+				<button className='button-rounded'>
+					<Play className='icon-button' />
+				</button>
+				<button className='button-rounded'>
+					<Reset className='icon-button' />
+				</button>
 			</div>
 		</div>
 	)
