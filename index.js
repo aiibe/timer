99681aca0f16f43e566af04e347e70ed254c0593
aiibe <souksavanh.sy@gmail.com>
2020-05-08 11:44:42 +0700

    save only files to be hosted

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './src/scss/style.scss'
-
-import App from './src/com/App'
-
-ReactDOM.render(<App />, document.getElementById('app'))
