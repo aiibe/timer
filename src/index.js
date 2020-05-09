d2690535c9dd68d074245cdff67db0af7ef9bba1
aiibe <souksavanh.sy@gmail.com>
2020-05-09 21:33:26 +0700

    moving source together in src/

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './src/scss/style.scss'
+import './scss/style.scss'
 
-import App from './src/com/App'
+import App from './com/App'
 
 ReactDOM.render(<App />, document.getElementById('app'))
