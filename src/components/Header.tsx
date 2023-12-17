9e2233dcfbfae003350863f47b7eeda5dd48bbc2
souksyp <souk@everblix.fr>
2023-12-17 14:50:25 +0100

    Support long text and refactor structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { Helmet } from "react-helmet";
+
+import { DEFAULT_TIME } from "../App";
+
+interface Props {
+  status: boolean;
+  currentTime: number;
+  min: string;
+  sec: string;
+}
+
+function Header(props: Props) {
+  const { status, currentTime, min, sec } = props;
+
+  return (
+    <Helmet defer={false}>
+      <meta charSet="utf-8" />
+      <title>
+        {status
+          ? `${min}:${sec} running`
+          : currentTime !== DEFAULT_TIME
+          ? `${min}:${sec} paused`
+          : "Pomoto"}
+      </title>
+      <link rel="canonical" href="https://aiibe.github.io/pomoto" />
+    </Helmet>
+  );
+}
+
+export default Header;
